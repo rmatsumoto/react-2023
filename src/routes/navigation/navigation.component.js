1b111804ff17d6ccd03d02e7404ab2f9949ff5eb
Ryota Matsumoto <ryotamatsumoto@Ryotas-Laptop.local>
2023-11-19 19:47:59 -0800

    added navbar + some assets

---
+++ src/routes/navigation/navigation.component.js
@@ -0,0 +1,22 @@
+import {Fragment} from "react";
+import {Outlet, Link} from "react-router-dom";
+import {ReactComponent as CrwnLogo} from "../../assets/crown.svg";
+import './navigation.styles.scss';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <nav className={'navigation'}>
+        <Link to={'/'} className={'logo-container'}>
+          <CrwnLogo className={'logo'} />
+        </Link>
+        <div className={'nav-links-container'}>
+          <Link className={'nav-link'} to={'/shop'}>Shop</Link>
+
+        </div>
+      </nav>
+      <Outlet />
+    </Fragment>
+  )
+}
+export default Navigation;
